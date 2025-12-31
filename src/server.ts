f2965ed9681861f6442f7da2b7e6e7df23212395
Biplob Hasan <your-email@example.com>
2025-12-31 21:11:53 +0600

    add initial project setup and post operation

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import app from "./app";
+import { prisma } from "./lib/prisma";
+
+const PORT = process.env.PORT || 3000;
+
+async function server() {
+  try {
+    await prisma.$connect();
+  } catch (error) {
+    console.error(error);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+server();
